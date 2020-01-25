e232923d26dc76d9079178decfcf167abd4b697c
brunoluz <8vm8hBRW>
2020-01-25 11:11:59 -0300

    Primeira versão

---
+++ src/test/java/br/com/bjsl/pocautenticacaojwtoauth2/PocAutenticacaoJwtOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bjsl.pocautenticacaojwtoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocAutenticacaoJwtOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
